a3d7f02aa5be4aee099a2b083e16fb088af72e31
Serdyukov Alexander <30432511+SerdyukovAlexander@users.noreply.github.com>
2024-10-09 12:08:45 +0500

    Задание 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,7 +1,9 @@
 package com.example.task02;
 
-public class Task02Main {
-    public static void main(String[] args) {
-
+public class Task02Main
+{
+    public static void main(String[] args)
+    {
+        TimeSpan timeSpan = new TimeSpan(3, 44, 200);
     }
 }
