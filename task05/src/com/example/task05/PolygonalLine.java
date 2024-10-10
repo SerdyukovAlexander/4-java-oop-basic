f9e89d6e343d3205fed7d6826569aef1e2979e29
Serdyukov Alexander <30432511+SerdyukovAlexander@users.noreply.github.com>
2024-10-10 22:28:48 +0500

    Задание 5

--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -1,46 +1,44 @@
 package com.example.task05;
+import java.util.ArrayList;
 
-/**
- * Ломаная линия
- */
-public class PolygonalLine {
+public class PolygonalLine
+{
+    private final ArrayList<Point> points;
 
-    /**
-     * Устанавливает точки ломаной линии
-     *
-     * @param points массив точек, которыми нужно проинициализировать ломаную линию
-     */
-    public void setPoints(Point[] points) {
-        // TODO: реализовать
+    public PolygonalLine()
+    {
+        points = new ArrayList<>();
     }
 
-    /**
-     * Добавляет точку к ломаной линии
-     *
-     * @param point точка, которую нужно добавить к ломаной
-     */
-    public void addPoint(Point point) {
-        // TODO: реализовать
+    public void setPoints(Point[] points)
+    {
+        this.points.clear();
+
+        for (Point point : points)
+        {
+            addPoint(point);
+        }
     }
 
-    /**
-     * Добавляет точку к ломаной линии
-     *
-     * @param x координата по оси абсцисс
-     * @param y координата по оси ординат
-     */
-    public void addPoint(double x, double y) {
-        // TODO: реализовать
+    public void addPoint(Point point)
+    {
+        points.add(new Point(point.getX(), point.getY()));
     }
 
-    /**
-     * Возвращает длину ломаной линии
-     *
-     * @return длину ломаной линии
-     */
-    public double getLength() {
-        // TODO: реализовать
-        throw new AssertionError();
+    public void addPoint(double x, double y)
+    {
+        points.add(new Point(x, y));
     }
 
+    public double getLength()
+    {
+        double length = 0.0;
+
+        for (int i = 1; i < points.size(); i++)
+        {
+            length += points.get(i - 1).getLength(points.get(i));
+        }
+
+        return length;
+    }
 }
--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -1,7 +1,12 @@
 package com.example.task05;
 
-public class Task05Main {
-    public static void main(String[] args) {
-
+public class Task05Main
+{
+    public static void main(String[] args)
+    {
+        PolygonalLine line = new PolygonalLine();
+        line.addPoint(0, 0);
+        line.addPoint(2, 3);
+        System.out.println(line.getLength());
     }
 }
