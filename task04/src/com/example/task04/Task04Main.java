95345d4372c6c80044980645872a4e64fff59faa
Serdyukov Alexander <30432511+SerdyukovAlexander@users.noreply.github.com>
2024-10-10 22:28:26 +0500

    Задание 4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,7 +1,15 @@
 package com.example.task04;
 
-public class Task04Main {
-    public static void main(String[] args) {
-
+public class Task04Main
+{
+    public static void main(String[] args)
+    {
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(6,6);
+        Line line = new Line(p1,p2);
+        Point p3 = new Point(3,3);
+        Point p4 = new Point(2,2);
+        System.out.println(line.isCollinearLine(p3));
+        System.out.println(line.isCollinearLine(p4));
     }
 }
